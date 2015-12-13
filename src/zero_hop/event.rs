ae1b11a1d1d994bb687db1d0cf2253725a5fa902
Dan Rammer <hamersaw@gmail.com>
2015-12-12 19:23:12 -0700

    fixed license to include capnproto authors

--- src/zero_hop/event.rs
+++ src/zero_hop/event.rs
@@ -2,7 +2,6 @@ use std::collections::BTreeMap;
 use std::net::SocketAddrV4;
 
 pub enum Event {
-    LookupMsgEvent(u64),
     LookupTableMsgEvent(BTreeMap<u64, SocketAddrV4>),
     RegisterTokenMsgEvent(u64, SocketAddrV4),
 }
